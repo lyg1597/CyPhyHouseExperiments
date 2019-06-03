989af3956fb8a6bf0ff2563782209b8b49373435
zitongc2 <zitongc2@illinois.edu>
2019-06-03 15:43:57 -0500

    file submission

--- src/Deployment/controller.py
+++ src/Deployment/controller.py
@@ -58,6 +58,34 @@ def git_transfering( address , url ):
     sender.close()
     
 
+def execute_command_w_feedback( address: tuple , command: str ):
+
+    sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sender.connect( (address, 60652) )
+    sender.send(bytes('EXF' + command, "utf-8"))
+    
+    #wait for resault for 10 sencond 
+    sender.settimeout(10.0)
+    try:
+        response, device_address = sender.recvfrom(1024)
+        response = response.decode("utf-8")
+    except:
+        print("[ERROR]: response connection time out, something is woring on the device, SSH for futher information")
+        sender.close()
+        return
+   
+    if(response == "SUCCESS"):
+        print("[INFO]: commmand execute successed in device ", address)
+    elif(response[0:4] == "FAIL"):
+        print("[ERROR]: commmand execute FAIL in device ", address)
+        print(" \n\n ------------------------------------------------------")
+        print("[ERROR]: The device returns : " + response[3:-1])
+        print(" \n\n\n")
+    else:
+        print("[ERROR]: unknow response code, contact administrator.")
+
+    sender.close()
+
 def execute_program(address, command):
     '''
     this function handles command execution
@@ -66,6 +94,24 @@ def execute_program(address, command):
     sender.connect( (address, 60652) )
     sender.send(bytes('EXC' + "  "+ command, "utf-8"))
     sender.close()
+
+def device_selection(device_list):
+    print("Which of the following devices do you want to tranfer file using GIT? ")
+    print("\n ------------------------ Device List ------------------------")
+    for index, attributes in device_list.items():
+        print(index, " : name" + attributes[0] + "\t\t\t IP:" + attributes[1] + "\t\t Status:" + attributes[2] )
+    print(" -------------------------------------------------------------\n")
+
+    index_list = input("Input the device index, separate by space >>> ").split(' ')
+    print('\n')
+    try:
+        index_list = [int(x) for x in index_list if x != '']
+        index_list = list(set(index_list))
+    except:
+        print("[ERROR]: input not valid")
+        return 0
+
+    return index_list
 
 # ---------------------------------------------------------------------------------------------
 # The following is the main function 
@@ -84,8 +130,8 @@ while True:
     elif( command[0:4] == "list" ):
         update_device(device_list)
 
-    elif( command[0:3] == "git" ): 
-        print("Which of the following devices do you want to tranfer file using GIT? ")
+    elif( command[0:9] == "execute-f" ): 
+        print("Which of the following devices do you want to exete command and want feed back? ")
         print("\n ------------------------ Device List ------------------------")
         for index, attributes in device_list.items():
             print(index, " : name" + attributes[0] + "\t\t\t IP:" + attributes[1] + "\t\t Status:" + attributes[2] )
@@ -101,8 +147,8 @@ while True:
             continue
 
         for index in index_list:
-            print("[GIT]: Sending command to " + str(device_list[index][1]))
-            git_transfering( device_list[index][1] , command[3:len(command)] )
+            print("[EXF]: Sending command to " + str(device_list[index][1]))
+            execute_command_w_feedback( device_list[index][1] , command[9:len(command)] )
 
     elif( command[0:7] == "execute" ):
         print("Which of the following devices do you want to tranfer file using GIT? ")
@@ -123,6 +169,33 @@ while True:
         for index in index_list:
             print("[EXC]: Sending command to " + str(device_list[index][1]))
             execute_program( device_list[index][1] , command[7:len(command)] )
+    
+    elif( command[0:4] == "kill"):
+        index_list = device_selection(device_list)
+        if( index_list == 0):
+            continue
+        argument = "" ##########need attention
+        for index in index_list:
+            print("[KILL]: Sending command to " + str(device_list[index][1]))
+            execute_command_w_feedback( device_list[index][1] , argument )
+        
+    elif( command[0:3] == "run"):
+        index_list = device_selection(device_list)
+        if( index_list == 0):
+            continue
+        argument = "" ##########need attention       
+        for index in index_list:
+            print("[RUN]: Sending command to " + str(device_list[index][1]))
+            execute_program( device_list[index][1] , argument )
+
+    elif( command[0:6] == "gitrun"):
+        index_list = device_selection(device_list)
+        if( index_list == 0):
+            continue
+        argument = "" ##########need attention        
+        for index in index_list:
+            print("[GITRUN]: Sending command to " + str(device_list[index][1]))
+            execute_program( device_list[index][1] , argument )
 
     else:
         print("[ERROR]: command not vaild")
