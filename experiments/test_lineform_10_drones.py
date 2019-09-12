eb2d8d81c59d28a0f09cfc6f51106a1a88d08b83
Hebron Taylor <hdt2@illinois.edu>
2019-09-11 20:20:21 -0500

    linefrom (6,8,10,12) drones data collection complete, added config files and test files

---
+++ experiments/test_lineform_10_drones.py
@@ -0,0 +1,70 @@
+from src.apps.lineform import LineForm
+from src.config.configs import AgentConfig, default_qc_moat_config, default_car_moat_config
+from src.functionality.base_mutex_handler import BaseMutexHandler
+from src.motion.rrt_drone import RRT as RRT_DRONE
+from src.motion.rrt_car import RRT as RRT_CAR
+from src.motion.moat_test_drone import MoatTestDrone
+from src.motion.moat_test_car import MoatTestCar
+
+
+import multiprocessing as mp
+
+
+def run_app(app_class, agent_config, moat_config):
+    app = app_class(agent_config, moat_config)
+    try:
+        app.start()
+        app.join()
+    except KeyboardInterrupt:
+        app.stop()
+    finally:
+        app.join()
+
+
+def main():
+    leader = 2
+    plist = list(range(2000, 2010))
+
+    app_list = []
+    for port in plist:
+        pid = port - 2000
+        is_leader = (pid == leader)
+        agent_config = AgentConfig(
+            pid, len(plist), "",
+            rport=port, plist=plist, mh= BaseMutexHandler,
+            is_leader=is_leader, mhargs=[is_leader, pid]
+        )
+        agent_config.moat_class = MoatTestDrone
+
+        botname = 'drone' + str(pid)
+        moat_config = default_qc_moat_config(botname)
+        moat_config.planner = RRT_DRONE()
+        moat_config.rospy_node = botname + '/waypoint_node'
+
+        app_list.append(
+            mp.Process(
+                name=botname,
+                target=run_app,
+                args=(LineForm, agent_config, moat_config)
+            )
+        )
+
+    for app in app_list:
+        app.start()
+
+    try:
+        for app in app_list:
+            app.join()
+    except KeyboardInterrupt:
+        print("User sent SIGINT")
+        for app in app_list:
+            app.join(2.0)  # Wait for a while
+    finally:
+        for app in app_list:
+            if app.is_alive():
+                print(app.name, "is still alive. Escalate to SIGTERM")
+                app.terminate()
+
+
+if __name__ == "__main__":
+    main()
